e6eb264628919e6e340954fa19a1c78d78239fef
Sindre Føring Devik <sindre.devik@gmail.com>
2013-04-22 15:34:51 +0200

    Increased robustness, added filtering options, cleaned up program arguments

--- fetch-links.py
+++ fetch-links.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # Fetches data files from given link as argument.
-# TODO: Parallelize and make recursive. Currently
-# skips subdirectories. Also increase robustness.
+# TODO: Currently skips subdirectories.
 from BeautifulSoup import BeautifulSoup
 import urllib
 import urllib2
@@ -11,39 +10,49 @@ import os.path
 import os
 import pprocess
 import time
+import argparse
+import re
 
-if len(sys.argv) != 2:
-	print "Fetches all links from url given"
-	print "Usage: ", sys.argv[0], " url"
-	exit(1)
-urlname = sys.argv[1]
-dirname = os.path.basename(urlname.strip('/'))
+parser = argparse.ArgumentParser()
+parser.add_argument("url", help="Fetches all links from url given")
+parser.add_argument("include", help="list include patterns here\nExample: include pdf: .*\.pdf")
+args = parser.parse_args()
+
+t = args.include.split(" ")
+tt = [ "(" + ti + ")" for ti in t ]
+restring = "|".join(tt)
+regexObj = re.compile(restring)
+
+dirname = os.path.basename(args.url.strip('/'))
+
 
 if not os.path.exists(dirname):
 	os.mkdir(dirname)
 else:
-	dirname = urlname
+	dirname = args.url
 	if not os.path.exists(dirname):
 		os.makedirs(dirname)
 
 os.chdir(dirname)
-html_page = urllib2.urlopen(urlname)
+html_page = urllib2.urlopen(args.url)
 soup = BeautifulSoup(html_page)
-links = soup.findAll('a')
+links = soup.findAll(name='a', attrs={'href': regexObj})
 
 def fetchLink(link):
-	urllib.urlretrieve(urlname + link.get('href'), link.get('href'))
+	print args.url
+	print link.get('href')
+	urllib.urlretrieve(link.get('href'), os.path.basename(link.get('href')))
 
 limit = len(links)
-#results = pprocess.pmap(fetchLink, links, limit=limit)
 i = 0
 n = len(links)
 
 tic = time.time()
 for link in links:
-	if str(link.get('href'))[-1] == '/':
+	l = link.get('href')
+	if str(l)[-1] == '/':
 		continue
-	print "fetching #", i, " of ", n, ": ", link.get('href')
+	print "fetching #", i, " of ", n, ": ", os.path.basename(link.get('href'))
 	fetchLink(link)
 	i += 1
 
